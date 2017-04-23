5fc6dd0498e0441d2166383cd2679a1b046f2595
grantpfeifer <grant.pfeifer5@gmail.com>
2017-04-23 15:50:17 -0400

    Javadoc

--- ReadingComp2/src/readingcomppackage/Person.java
+++ ReadingComp2/src/readingcomppackage/Person.java
@@ -5,31 +5,37 @@
  */
 package readingcomppackage;
 
-/**
+/** Stores the information of the user of the program
  *
- * @author grant
+ * @author Grant Pfeifer
+ * @version 1.0 - 23 April 2017
  */
 public class Person {
+    // A string for storing the person's name 
     private String studentName;
-    private int score;
     
+    /**
+     * Instantiates a new person object and sets the name
+     * @param name the name of the person
+     */
     public Person(String name){
         setName(name);
     }
     
+    /**
+     * Sets the name of the person
+     * @param n a string containing the name
+     */
     private void setName(String n){
         studentName = n;
     }
     
+    /**
+     * Used to return the name of the person
+     * @return a string containing the persons name
+     */
     public String getName(){
         return this.studentName;
     }
     
-    public void addScore(){
-        score++;
-    }
-    
-    public int getScore(){
-        return this.score;
-    }
 }
